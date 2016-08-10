7a6f4fad3814bc12478f71ce62b0ce9f1903ab68
Maksim Berjoza <torbjon@gmail.com>
2016-08-10 14:15:14 +0300

    changed example dummy data

--- examples/examples.js
+++ examples/examples.js
@@ -2,33 +2,66 @@ var InfogramAPI = require('../');
 
 var infogram = new InfogramAPI('apiKey', 'apiSecret');
 
-// var project_id = 'projectid';
+var params = {
+  theme_id: 308,
+  content: [
+    {
+      "type": "h1",
+      "text": "Testing API client"
+    },
+    {
+      "type": "body",
+      "text": "Lorem ipsum dolor sit amet..."
+    },
+    {
+      "type": "quote",
+      "text": "God does not play dice",
+      "author": "Albert Einstein"
+    },
+    {
+      "type": "chart",
+      "chart_type": "bar",
+      "data": [
+        [
+          ["apples", "today", "yesterday", "d. bef. yesterday"],
+          ["John", 4, 6, 7],
+          ["Peter", 1, 3, 9],
+          ["George", 4, 4, 3]
+        ]
+      ],
+      "settings": {
+        "width": 0.5,
+        "height": 300
+      }
+    },
+    {
+      "type": "map",
+      "territory": "world",
+      "data": [
+        {
+          "title": "Japan",
+          "value": 126434964,
+          "label": "Japan",
+          "group": "A",
+          "color": "#0000FF"
+        },
+        {
+          "title": "United States",
+          "value": 318968000,
+          "label": "USA",
+          "group": "A",
+          "color": "#FF0000"
+        }
+      ]
+    }
+  ]
+}
 
-// var params = {
-//   theme_id: 308,
-//   content: [
-//     {
-//       type: 'h2',
-//       text: 'Node you are awesome'
-//     },
-//     {
-//       type: 'chart',
-//       chart_type: 'bar',
-//       data: [
-//         ['apples', 'today', 'yesterday', 'd. bef. yesterday'],
-//         ['John', 4, 6, 7],
-//         ['Peter', 1, 3, 9],
-//         ['George', 4, 4, 3]
-//       ]
-//     },
-//   ]
-// }
-
-// infogram.createProject(params).then(function(data) {
-//   console.log(data);
-// }, function(err) {
-//   console.error(err);
-// });
+infogram.createProject(params).then(function(data) {
+  console.log(data);
+}, function(err) {
+  console.error(err);
+});
 
 // infogram.getProject(project_id).then(function(data) {
 //   console.log(data);
